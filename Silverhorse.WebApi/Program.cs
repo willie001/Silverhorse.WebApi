0c81f55793f7d583378ffb3d758880131fa0cea1
Willie Kruger <33948651+willie001@users.noreply.github.com>
2022-07-14 15:14:48 +0800

    Swagger Authorization

--- Silverhorse.WebApi/Program.cs
+++ Silverhorse.WebApi/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.OpenApi.Models;
 using Silverhorse.WebApi.CustomAuthentication;
 using Silverhorse.WebApi.SwaggerConfig;
 
@@ -13,7 +14,38 @@ builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddSwaggerGen(c =>
 {
-    c.OperationFilter<HeaderFilter>();
+    //c.OperationFilter<HeaderFilter>();
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+      {
+        {
+          new OpenApiSecurityScheme
+          {
+            Reference = new OpenApiReference
+              {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
+              },
+              Scheme = "oauth2",
+              Name = "Bearer",
+              In = ParameterLocation.Header,
+
+            },
+            new List<string>()
+          }
+        });
+
 });
 
 // Add Custom Authentication
